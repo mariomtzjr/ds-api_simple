ab065f58b9db6b798ddd552e5c7e08e88c71e281
mmartinez <mmartinez@creze.com>
2020-02-13 11:15:36 -0600

    Se crearon las primeras rutas.

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify
+from products import products
+
+app = Flask(__name__)  # aplicación de Flask
+
+"""Entregar datos
+Creación de rutas para servir datos (productos)
+Características:
+    - Permiten especificar los métodos HTTP
+"""
+
+
+@app.route('/ping')
+def index():
+    return jsonify({"message": "ruta de prueba"})
+
+
+# Por defecto, las rutas manejan petición GET
+@app.route('/products', methods=['GET'])
+def getProducts():
+    # return jsonify(products)
+    return jsonify({"products": products})  # Propiedad products
+
+
+# Mostrar producto en particular
+# @app.route('/priducts/<string:product_name>')
+@app.route('/products/<product_name>')
+def getProduct(product_name):
+    productsFound = [product for product in products
+        if product['name'] == product_name]
+    return jsonify({"product": productsFound})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=9091)  # Inicialización de la aplicación
---
+++ products.py
@@ -0,0 +1,7 @@
+# Productos a entregar a las aplicaciones cliente
+
+products = [
+    {"name": "laptop", "price": 800, "quantity": 5},
+    {"name": "mouse", "price": 40, "quantity": 10},
+    {"name": "monitor", "price": 300, "quantity": 3},
+]
